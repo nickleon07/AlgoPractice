56116de16df1909f7b817bbb6fef87617f362820
nickleon07 <nick.r.leon@gmail.com>
2022-02-10 10:23:41 -0800

    more

---
+++ Recursion - someRecursive.js
@@ -0,0 +1,20 @@
+// SAMPLE INPUT / OUTPUT
+// const isOdd = val => val % 2 !== 0;
+
+// someRecursive([1,2,3,4], isOdd) // true
+// someRecursive([4,6,8,9], isOdd) // true
+// someRecursive([4,6,8], isOdd) // false
+// someRecursive([4,6,8], val => val > 10); // false
+
+function someRecursive(arr, callback){
+  // add whatever parameters you deem necessary - good luck!
+  if (arr.length === 1) {
+      return callback(arr[0]);
+  } else {
+      if (callback(arr[0]) === true) {
+          return true;
+      } else {
+          return someRecursive(arr.slice(1), callback)
+      }
+  }
+}
